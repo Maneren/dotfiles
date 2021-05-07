3288377b715282d28f24fa69ab8d471f1452acf9
Maneren <49210777+Maneren@users.noreply.github.com>
2021-05-07 17:52:56 +0200

    update install script

--- install.sh
+++ install.sh
@@ -6,13 +6,33 @@ shopt -s dotglob
 shopt -s extglob
 
 for dir in "$CURRENT_DIR"/!(".git"|".."|"."); do
+    
     test -d "$dir" || continue
     echo "Setting up $(basename "$dir")"
     
     for item in "$dir"/!(".."|"."); do
-        # test -f "$item" || continue
-        echo "$HOME/$(basename "$item")"
-        rm -ir "${HOME:?}/$(basename "$item")"
+        name=$(basename "$item")
+        
+        if [[ -f "$item" ]]; then
+            echo '~'"/$name => $item"
+        else
+            echo '~'"/$name/ => $item"
+        fi
+        
+        if [ "$HOME/$name" -ef "$item" ]; then
+            continue
+        fi
+        
+        if [[ -L "$HOME/$name" || -f "$HOME/$name" ]]; then
+            rm -i "${HOME:?}/$name"
+        fi
+        
+        if [[ -d "$HOME/$name" ]]; then
+            rm -ir "${HOME:?}/$name"
+        fi
+        
         ln -s "$item" ~/
     done
+    
+    printf "\n"
 done
