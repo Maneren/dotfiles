f95502abb0c5cf37e3f8cd956c0fddabaffe1f61
Maneren <49210777+Maneren@users.noreply.github.com>
2021-11-01 20:45:59 +0100

    removed unused and/or obsolete aliases

--- zsh/zsh/aliases.zsh
+++ zsh/zsh/aliases.zsh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 
-alias zshrc='nano ~/.zshrc'
-
 pyc() {
     python3 -c "print($1)"
 }
@@ -14,25 +12,13 @@ alias qr='qrencode -m 2 -t utf8 <<< "$1"'
 alias record='asciinema rec'
 alias myip='curl http://ipecho.net/plain; echo'
 alias passgen='pwgen -cnys1 "--remove-chars=;['\''\\\"" 16 3'
-alias allpkgs='dpkg --get-selections | grep -v deinstall'
-alias toppkgs='apt-mark showmanual |\
-                sort |\
-                grep -v -F -f <(apt show $(apt-mark showmanual) 2> /dev/null |\
-                grep -e ^Depends -e ^Pre-Depends |\
-                sed '\''s/^Depends: //; s/^Pre-Depends: //; s/(.*)//g; s/:any//g'\'' |\
-                tr -d '\'',|'\'' |\
-                tr '\'' '\'' '\''\n'\''|\
-                grep -v ^$ |\
-                sort -u)'
 alias dropcache='sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"'
-alias youtube-dl="youtube-dl --add-metadata -i -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4"
 
 quote() {
     pyc '__import__("re").sub("'\''", "'\''\\\\'\'\''", r"""'$1'""")'
     #                        ("  ' ", "  ' \ \   ' ' ", r""" $1 """)
     #                        ("'",    "'\\''",          r"""$1""")
 }
-alias load='<'
 
 alias ls='lsd --group-dirs first'
 alias l='ls -l'
@@ -41,7 +27,6 @@ alias lla='ls -lA'
 alias lt='ls --tree'
 alias lls='l --total-size -S'
 alias las='lls -A'
-alias /='cd /'
 alias cls='clear'
 
 alias gitc='git clone'
@@ -78,14 +63,7 @@ mkcd() {
 }
 
 alias pip='pip3'
-alias newpyenv='virtualenv env && source ./env/bin/activate'
-alias pinstall='pip install'
-
-# load platform specific aliases
-alias zshconfig='code ~/git-repos/dotfiles/zsh'
-
 alias diskspace='sudo ncdu --exclude /mnt --exclude /proc /'
 alias kspenv='. /home/paul/Programming/KSP/interface/kspenv/bin/activate'
 alias rustscan='docker run -it --rm --ulimit nofile=80000:80000 --name rustscan rustscan/rustscan:2.0.0 --scan-order "Random"'
 alias nikto='docker run --rm --ulimit nofile=15000:15000 --name nikto sullo/nikto'
-alias exiftool='~/.local/bin/exiftool'
