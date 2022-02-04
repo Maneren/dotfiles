7c95f57862aa01e6a84dc843e057b542b149d5ae
Maneren <49210777+Maneren@users.noreply.github.com>
2022-02-04 23:50:43 +0100

    add link test script

--- test-install.sh
+++ test-install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 docker run -w /dotfiles -v "$(pwd):/dotfiles" manjarolinux/base ./install.sh
+# docker run -w /dotfiles -v "$(pwd):/dotfiles" manjarolinux/base sh -c "curl -fsSL bit.ly/maneren-install | bash -"
