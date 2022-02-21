f579d51d3438979a695ee3643292385dc89d48ff
Maneren <49210777+Maneren@users.noreply.github.com>
2022-02-21 23:58:13 +0100

    remove git check and format net-install

--- net-install.sh
+++ net-install.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-if ! command -v git >/dev/null 2>&1; then
-  echo "Git must be installed"
-  exit 1
-fi
-
-git clone https://github.com/Maneren/dotfiles ~/git-repos/dotfiles
-cd ~/git-repos/dotfiles || exit 1
-./install.sh
+git clone https://github.com/Maneren/dotfiles ~/git-repos/dotfiles \
+&& cd ~/git-repos/dotfiles \
+&& ./install.sh
