a8d1eee93bfbe542cd3cfca89ab2b92619195d96
Maneren <49210777+Maneren@users.noreply.github.com>
2022-03-20 23:08:10 +0100

    remove test with download

--- test-install.sh
+++ test-install.sh
@@ -7,8 +7,3 @@ docker run \
 -v "$(pwd):/home/test_user/Downloads/dotfiles" \
 dotfiles \
 ./install.sh
-
-docker run \
--w /home/test_user \
-dotfiles \
-sh -c "curl -fsSL rebrand.ly/maneren-install | sh -"
