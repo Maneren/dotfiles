d659fbb47562af0749791a8b43ff65d9af032f8e
Maneren <49210777+Maneren@users.noreply.github.com>
2022-09-20 23:19:24 +0200

    formatting

--- install.sh
+++ install.sh
@@ -29,12 +29,12 @@ echo-red() {
 
 git_clone() {
   local name="${1#*/}"
-  
+
   folder=$2
   if [ -z "$folder" ]; then
     folder=$name
   fi
-  
+
   if [ -d "$name" ]; then
     echo-red "$name already downloaded"
   else
@@ -43,12 +43,12 @@ git_clone() {
   fi
 }
 
-update () {
+update() {
   echo-red "Updating system..."
   sudo pacman -Syyuu --noconfirm
 }
 
-packages () {
+packages() {
   echo-red "Installing packages..."
   pm asciinema bat bpytop curl diff-so-fancy exa fd git jq make nano neovim python3 python-pip wget
   install_zinit
@@ -56,24 +56,24 @@ packages () {
   install_rustup
   install_tmux
   install_yay
-  
+
   [ "$(uname -m)" = aarch64 ] && packages_arm
   [ "$(uname -m)" = x86_64 ] && packages_x86
 }
 
-packages_x86 () {
+packages_x86() {
   echo-red "Installing x86 packages..."
-  
+
   pm powerline-go
-  
+
   home_packages
   install_fonts
   keyboard
 }
 
-packages_arm () {
+packages_arm() {
   echo-red "Installing arm packages..."
-  
+
   ya powerline-go-git
 }
 
@@ -89,20 +89,20 @@ install_yay() {
   yay -Y --devel --combinedupgrade --batchinstall --save
 }
 
-install_tmux () {
+install_tmux() {
   echo-red "Installing tmux..."
   pm tmux
   mkdir -p ~/.tmux
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
 
-home_packages () {
+home_packages() {
   echo-red "Installing home packages..."
   pm libreoffice onlyoffice-desktopeditors steam-native peek bitwarden thunderbird qcopy flameshot guake gwe
   install_discord
 }
 
-install_discord () {
+install_discord() {
   echo-red "Installing Discord..."
   pm discord-canary
   (
@@ -114,24 +114,24 @@ install_discord () {
   )
 }
 
-install_node () {
+install_node() {
   echo-red "Installing NodeJS and pnpm..."
   pm nodejs
-  
+
   sudo corepack enable
-  
+
   local pnpm_version
   pnpm_version=$(curl -sL https://api.github.com/repos/pnpm/pnpm/releases/latest | jq -r ".tag_name")
-  
+
   sudo corepack prepare "pnpm@$pnpm_version" --activate
-  
+
   (
     cd ~/ || exit 1
     pnpm config set store-dir ~/.cache/pnpm-store
   )
 }
 
-install_rustup () {
+install_rustup() {
   echo-red "Installing rustup..."
   pm rustup
   rustup self upgrade-data
@@ -140,107 +140,107 @@ install_rustup () {
   rustup default stable
 }
 
-install_fonts () {
+install_fonts() {
   echo-red "Installing fonts..."
   ya ttf-twemoji ttf-segoe-ui-variable nerd-fonts-cascadia-code nerd-fonts-jetbrains-mono
   sudo ln -sf /usr/share/fontconfig/conf.avail/75-twemoji.conf /etc/fonts/conf.d/75-twemoji.conf
   (
     local target="$HOME/git-repos/Iosevka"
-    
+
     git clone --depth 1 https://github.com/be5invis/Iosevka "$target"
-    
+
     ln -s "$(pwd)/font/Iosevka/private-build-plans.toml" "$target"
-    
+
     cd "$target" || return
-    
+
     pnpm i && pnpm build -- ttf::iosevka-custom
-    
+
     sudo mkdir /usr/local/share/fonts
     sudo mkdir /usr/local/share/fonts/iosevka-custom/
-    
+
     sudo mv dist/* /usr/local/share/fonts/iosevka-custom/
   )
 }
 
-install_zinit () { # Installs zsh, oh-my-zsh and plugins
+install_zinit() { # Installs zsh, oh-my-zsh and plugins
   echo-red "Installing zsh and zinit..."
   pm zsh
-  
+
   mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
   git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git"
 }
 
-dotfiles () {
+dotfiles() {
   echo-red "Installing dotfiles..."
   mkdir -p ~/.dotfiles
-  
+
   local base
   base="$(pwd)"
-  
+
   local dirs
   dirs=(bash git zsh tmux) # folders to be linked
-  
+
   for dir in "${dirs[@]}"; do
     echo-red "Setting up $(basename "$dir")"
-    
+
     local items
     items=$(find "$dir" -maxdepth 1 -mindepth 1)
-    
+
     for item in $items; do
       name="$(basename "$item")"
-      
+
       item="$base/$item"
-      
+
       if [ -f "$item" ]; then
         local target="$HOME/$name"
-        
+
         # if already correctly linked continue
         [ "$target" -ef "$item" ] && continue
-        
+
         echo '~'"/$name => $item"
-        
+
         # if there is something else, prompt user to delete it
         [ -d "$target" ] && rm -ir "$target"
         [[ -L "$target" || -f "$target" ]] && rm "$target"
-        
+
         ln -s "$item" ~/
-        elif [ -d "$item" ]; then
-        
+      elif [ -d "$item" ]; then
+
         local target="$HOME/.dotfiles/$name"
-        
+
         # if already correctly linked continue
         [ "$target" -ef "$item" ] && continue
-        
+
         echo '~'"/.dotfiles/$name/ => $item"
-        
+
         # if there is something else, prompt user to delete it
         [ -d "$target" ] && rm -ri "$target"
         [[ -L "$target" || -f "$target" ]] && rm -i "$target"
-        
+
         ln -s "$item" "$target"
       fi
     done
-    
+
     printf "\n"
   done
 }
 
-init () {
+init() {
   echo-red "Initializing..."
   mkdir -p ~/.local/bin ~/.local/shared ~/git-repos
 }
 
-keyboard () {
+keyboard() {
   echo-red "Installing keyboard layout..."
   if
-  [ -d /usr/share/X11/xkb/symbols ] &&
-  [ -f /usr/share/X11/xkb/rules/evdev.xml ] &&
-  [ ! -e /usr/share/X11/xkb/symbols/sexy_cz ]
+    [ -d /usr/share/X11/xkb/symbols ] &&
+      [ -f /usr/share/X11/xkb/rules/evdev.xml ] &&
+      [ ! -e /usr/share/X11/xkb/symbols/sexy_cz ]
   then
     echo-red "Installing xkb layout - log out required"
-    
+
     sudo ln -sf "$(pwd)/keyboard/sx" /usr/share/X11/xkb/symbols/
-    
+
     local layout_text="
     <layout>
     <configItem>
@@ -253,7 +253,7 @@ keyboard () {
     </configItem>
     <variantList/>
     </layout>"
-    
+
     local modified
     modified=$(awk -v r="$layout_text\n</layoutList>" '{gsub(/<\/layoutList>/,r)}1' /usr/share/X11/xkb/rules/evdev.xml)
     echo "$modified" | sudo tee /usr/share/X11/xkb/rules/evdev.xml >/dev/null
@@ -262,10 +262,10 @@ keyboard () {
   fi
 }
 
-change_shell () {
+change_shell() {
   if [ ! "$(basename -- "$SHELL")" = zsh ]; then
     echo-red "Switching default shell to zsh"
-    
+
     (
       zsh="/bin/zsh"
       export SHELL=$zsh
